2a37ec2205c03a438f300758aeca1d8512975f3e
HeisenbergTN <nizar.amamou@gmail.com>
2023-06-09 15:02:28 +0100

    test

--- data_base.py
+++ data_base.py
@@ -1,6 +1,32 @@
-import pyodbc
-server = '127.0.0.1'
-database='MileBix'
-username='admin'
-cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username)
-print(cnxn)                      
+import mysql.connector
+
+# Establish a connection to the MySQL server
+cnx = mysql.connector.connect(
+    host='localhost',  # Replace with the actual hostname
+    user='root',  # Replace with your MySQL username
+    password='',  # Replace with your MySQL password
+    database='MileBix'  # Replace with the name of your MySQL database
+)
+
+# Create a cursor object to interact with the database
+cursor = cnx.cursor()
+
+# Execute a SELECT query
+query = "SELECT * FROM table1"
+cursor.execute(query)
+
+# Fetch all rows returned by the query
+rows = cursor.fetchall()
+
+# Process the rows
+for row in rows:
+    # Access individual columns using row[column_index]
+    
+    column1_value = row[0]
+    column2_value = row[1]
+    print(str(column1_value)+" | "+str(column2_value))
+    # ...
+
+# Close the cursor and the connection
+cursor.close()
+cnx.close()
